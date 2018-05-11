6514d1cf1113fb98ca82b7cbb82beebb2a01a196
DJLionheart <aundario@gmail.com>
2018-05-11 10:33:40 -0600

    moving search results to redux

--- src/components/buttons/AddBtn/AddBtn.js
+++ src/components/buttons/AddBtn/AddBtn.js
@@ -1,36 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import filter from 'lodash/filter';
 
 import IconButton from 'material-ui/IconButton';
 import PlaylistAdd from '@material-ui/icons/PlaylistAdd';
-import { get_playlists } from '../../../ducks/users';
 
-
-function AddBtn(props) {
-    const { track_id, btnFunc } = props;
-    const { playlists, current_index, userid } = props.user_data
-
-
-    const onPlaylist = filter( playlists[current_index].tracks, {track_id: track_id})
-    console.log('On playlist: ', onPlaylist)
-    let color = '';
-
-    if(onPlaylist.length >= 1 ) {
-        color = 'secondary';
-    } else {
-        color = 'default';
+class AddBtn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: 'default'
+        }
     }
 
+    componentDidMount(props) {
+        const { track_id } = this.props;
+        const { playlists, current_index } = this.props.user_data
+        const onPlaylist = filter( playlists[current_index].tracks, {track_id: track_id})
+        if(onPlaylist.length >= 1 ) {
+            this.setState({
+                color: 'secondary'
+            })
+        }
+    }
 
-    return(
-        <IconButton 
-            aria-label="Add to playlist" 
-            color={ color } onClick={ () => btnFunc(track_id).then( res => get_playlists(userid)) }
-        >
-            <PlaylistAdd/>
-        </IconButton>   
-    )
+    render() {
+        const { track_id, btnFunc } = this.props;
+        const { color } = this.state;
+
+        return(
+            <IconButton 
+                aria-label="Add to playlist" 
+                color={ color } onClick={ () => btnFunc(track_id) }
+            >
+                <PlaylistAdd/>
+            </IconButton>   
+        )
+    }
 }
 
 function mapStateToProps(state) {
@@ -39,4 +45,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps, { get_playlists })(AddBtn);
+export default connect(mapStateToProps)(AddBtn);
--- src/ducks/search.js
+++ src/ducks/search.js
@@ -1,13 +1,30 @@
+import axios from 'axios'
+
 const initialState = {
     search_input: '',
     search_type: 'bpm',
-    results: []
+    results: [],
     sortBy: 'bpm',
     sortDirection: 'asc',
     invertDirection: {'asc': 'desc', 'desc': 'asc'}
 }
 
- const HANDLE_SORT = 'HANDLE_SORT';
+const HANDLE_SORT = 'HANDLE_SORT'
+    , SORT_RESULTS = 'SORT_RESULTS'
+    , GET_RESULTS = 'GET_RESULTS'
+    , GET_INPUT = 'GET_INPUT'
+    , GET_TYPE = 'GET_TYPE';
+
+const {
+     REACT_APP_SEARCH
+} = process.env;
+
+export function sort_results(sort) {
+    return {
+        type: SORT_RESULTS,
+        payload: sort
+    }
+}
 
 export function reduxSort(column, direction) {
     return {
@@ -19,11 +36,55 @@ export function reduxSort(column, direction) {
     }
 }
 
+export function get_input(e) {
+    return {
+        type: GET_INPUT,
+        payload: e.target.value
+    }
+}
+
+export function get_type(e) {
+    return {
+        type: GET_TYPE,
+        payload: e.target.value
+    }
+}
+
+export function get_results(searchType, searchInput) {
+
+    let results = axios.get(`${REACT_APP_SEARCH}?type=${ searchType }&search=${ searchInput }`).then( res => {
+        console.log('Search results: ', res.data)
+
+        return res.data
+    }).catch( err => console.log('DB search error: ', err))
+    
+    return {
+        type: GET_RESULTS,
+        payload: results
+    }
+}
+
 export default function search(state = initialState, action) {
     switch( action.type ) {
 
+        case SORT_RESULTS:
+            const { sortBy, sortDirection, invertDirection } = state;
+            let direction = '';
+            sortBy === action.payload ? direction = invertDirection[sortDirection] : direction = 'asc'
+
+            return Object.assign({}, state, {sortBy: action.payload, sortDirection: direction});
+
         case HANDLE_SORT:
             return Object.assign({}, state, {sortBy: action.payload.sortBy, sortDirection: action.payload.sortDirection})
+
+        case GET_INPUT:
+            return Object.assign({}, state, {search_input: action.payload});
+
+        case GET_TYPE:
+            return Object.assign({}, state, {search_type: action.payload});
+
+        case GET_RESULTS:
+            return Object.assign({}, state, {results: action.payload});
 
         default:
             return state
